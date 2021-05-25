1e18a90d9bf6fb5757f143f0e167bbb6f5998d30
HanSeoungJun <67749318+HanSeongJun@users.noreply.github.com>
2021-05-25 20:19:55 +0900

    JDBC Test

---
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,61 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional // Test 에서 DB를 사용 시 Test가 끝나면 Test의 데이터들을 다시 롤백 시켜준다.
+class MemberServiceIntegrationTest {
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("spring");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring"); // 같은 이름을 가진 회원을 만들기
+
+        // when
+        memberService.join(member1);
+        // memberService.join(member2); // 이름이 같으므로 중복 회원 오류 발생
+
+        // then
+        IllegalStateException e =
+                assertThrows(IllegalStateException.class,
+                        () -> memberService.join(member2)); // 예외가 발생해야 한다.
+
+        // 검증
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+}
