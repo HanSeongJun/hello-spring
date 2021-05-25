e595b789d261cf12681bc82428ebaf4682d29c8f
HanSeoungJun <67749318+HanSeongJun@users.noreply.github.com>
2021-05-25 22:13:18 +0900

    Jpa

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 회원을 식별할 수 있는 값
     private String name; // 이름
 
